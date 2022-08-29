e068c5213d6e815e1aa588a3a84af88cedb406b2
Edison Padilla <edisojp@gmail.com>
2022-08-29 12:13:23 -0400

    fix: remove some headers and add Access-Control-Allow-Origin

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -15,8 +15,9 @@ enum StatusCode {
 const headers: Readonly<Record<string, string | boolean>> = {
   Accept: 'application/json',
   'Content-Type': 'application/json; charset=utf-8',
-  'Access-Control-Allow-Credentials': true,
-  'X-Requested-With': 'XMLHttpRequest',
+  // 'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Origin': '*',
+  // 'X-Requested-With': 'XMLHttpRequest',
 };
 
 // We can use the following function to inject the JWT token through an interceptor
